b2873ab2042446d7f4e0ebfeefc0fae94afcb54a
KamalSemaan <kamalsemaan2@gmail.com>
2026-05-10 19:14:51 +0100

    finished task 2 files

---
+++ Task 2/ros2_ws/build/rrrrr_finger/CMakeFiles/rrrrr_finger_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for rrrrr_finger_uninstall.
+# This may be replaced when dependencies are built.
---
+++ Task 2/ros2_ws/build/scara_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/lunet/kk0171/ros2_ws/install/scara_pkg/share/scara_pkg/package.sh"
